b39a0b8e611a4b8febbc0ac8a97e7023a632dd61
Austinet <austinetsplendour@gmail.com>
2022-06-23 06:44:16 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let fullName = "Udhe Austine Ogaga";
+let height = 5.3;
+let country = "Nigeria";
+
+//printing to the console
+console.log(`My name is ${fullName}, I'm ${height}ft tall, and I'm from ${country}`);
+
+//printing using alert
+alert(`My name is ${fullName}, I'm ${height}ft tall, and I'm from ${country}`);
+
+//printing to screen using document.write
+document.write("NAME: " + fullName +"<br>");
+document.write("HEIGHT: " + height + "ft." +"<br>");
+document.write("COUNTRY: " + country);
